6852fea008129b971e4484dc51255b08a6dbf79f
Willian Krause <krause.willian@gmail.com>
2018-12-29 23:57:56 -0200

    Register screen (#17)
    * Register screen
    
    * Remove uneeded code

--- src/scenes/Login/index.js
+++ src/scenes/Login/index.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import { createStackNavigator } from 'react-navigation';
 import { Toast } from 'native-base';
 import { GoogleSignin } from 'react-native-google-signin';
-import { AccessToken, LoginManager } from 'react-native-fbsdk';
+import { AccessToken, LoginManager , GraphRequestManager, GraphRequest } from 'react-native-fbsdk';
 import I18n from '../../i18n';
 import firebase from '../../firebase';
+import NavigationService from '../../services/Navigation';
 
 import LoginScreen, { ROUTE_NAME as LOGIN_ROUTE } from './screens/Login/Login';
+import RegisterScreen, { ROUTE_NAME as REGISTER_ROUTE } from './screens/Register/Register';
 
 const LoginStack = createStackNavigator(
   {
     [LOGIN_ROUTE]: LoginScreen,
+    [REGISTER_ROUTE]: RegisterScreen,
   },
   {
     initialRouteName: LOGIN_ROUTE,
@@ -33,9 +36,9 @@ class Login extends Component {
 
   loginOrRegisterWithGoogle = async () => {
     try {
-      const { idToken, accessToken } = await GoogleSignin.signIn();
+      const { idToken, accessToken, user: { email } } = await GoogleSignin.signIn();
       const credential = firebase.authProviders.GoogleAuthProvider.credential(idToken, accessToken);
-      await this.loginToFirebase(credential);
+      await this.loginToFirebase(credential, email);
     } catch (err) {
       this.handleExternalError(err);
     }
@@ -49,11 +52,33 @@ class Login extends Component {
       }
 
       const { accessToken } = await AccessToken.getCurrentAccessToken();
+
+      const { email } = await this.getUserEmailFromFacebook(accessToken);
+
       const credential = firebase.authProviders.FacebookAuthProvider.credential(accessToken);
-      await this.loginToFirebase(credential);
+      await this.loginToFirebase(credential, email);
     } catch (err) {
       this.handleExternalError(err);
     }
+  }
+
+  getUserEmailFromFacebook = (accessToken) => {
+    return new Promise((resolve, reject) => {
+      const callback = (error, result) => {
+        if (error) {
+          return reject(error);
+        }
+        return resolve(result);
+      }
+
+      const graphRequest = new GraphRequest('/me', {
+        parameters: { fields: { string: 'email' } },
+        accessToken,
+      }, callback)
+
+      const graphManager = new GraphRequestManager();
+      graphManager.addRequest(graphRequest).start();
+    });
   }
 
   handleUserCreation = (user, displayName, photoUrl) => {
@@ -68,7 +93,7 @@ class Login extends Component {
     ]);
   }
 
-  loginToFirebase = async (credential) => {
+  loginToFirebase = async (credential, email) => {
     try {
       const { user, additionalUserInfo: {
         isNewUser,
@@ -81,19 +106,34 @@ class Login extends Component {
         await this.handleUserCreation(user, name, picture);
       }
     } catch (err) {
-      this.handleFirebaseError(err);
+      this.handleFirebaseError(err, email);
     }
   }
 
-  handleExternalError = err => {
+  register = async ({ name, email, password }) => {
+    try {
+      const { user } = await firebase.auth.createUserWithEmailAndPassword(email, password);
+
+      await this.handleUserCreation(user, name, '');
+    } catch (err) {
+      this.handleFirebaseError(err, email);
+    }
+  }
+
+  handleExternalError = () => {
     Toast.show({
       text: I18n.t('login.genericError'),
       duration: 5000,
     });
   }
 
-  handleFirebaseError = err => {
+  handleFirebaseError = (err, email) => {
     const { code, message } = err;
+
+    if (code === 'auth/account-exists-with-different-credential') {
+      return this.checkOtherMethods(email, code);
+    }
+
     const translated = I18n.t(`firebase.${code}`);
     Toast.show({
       text: translated === code ? message : translated,
@@ -101,13 +141,47 @@ class Login extends Component {
     });
   }
 
+  checkOtherMethods = async (email, code) => {
+    const methods = await firebase.auth.fetchSignInMethodsForEmail(email);
+    Toast.show({
+      text: I18n.t(`firebase.${code}`, methods.join(', ')),
+      duration: 5000,
+    });
+  }
+
+  goTo = (routeName, params) => {
+    let allowNavigation = false;
+    let defaultParams;
+
+    switch (routeName) {
+      case REGISTER_ROUTE:
+        allowNavigation = true;
+        defaultParams = {
+          registerWithGoogle: this.loginOrRegisterWithGoogle,
+          registerWithFacebook: this.loginOrRegisterWithFacebook,
+          register: this.register,
+          goToLogin: () => this.goTo(LOGIN_ROUTE),
+        };
+        break;
+      case LOGIN_ROUTE:
+        allowNavigation = true;
+        break;
+    }
+
+    if (allowNavigation) {
+      return NavigationService.navigate(routeName, params || defaultParams);
+    }
+  }
+
   render() {
     return (
       <LoginStack
+        ref={navigatorRef => NavigationService.setTopLevelNavigator(navigatorRef)}
         screenProps={{
           login: this.login,
-          loginOrRegisterWithGoogle: this.loginOrRegisterWithGoogle,
-          loginOrRegisterWithFacebook: this.loginOrRegisterWithFacebook,
+          loginWithGoogle: this.loginOrRegisterWithGoogle,
+          loginWithFacebook: this.loginOrRegisterWithFacebook,
+          goToRegister: () => this.goTo(REGISTER_ROUTE),
         }}
       />
     );
